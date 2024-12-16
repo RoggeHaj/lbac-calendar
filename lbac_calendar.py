615a09b06574c95d5972a5c21b80a23d070df837
Roger Magnesved <roger.magnesved@gmail.com>
2024-12-16 09:39:22 +0100

    Add optional section and paragraph to events

--- lbac_calendar.py
+++ lbac_calendar.py
@@ -10,10 +10,12 @@ from dateutil.relativedelta import relativedelta
 class meeting_event():
     """Holds a date and a description"""
 
-    def __init__(self, date, descr, offset_months = None, offset_weeks = None, offset_days = None):
+    def __init__(self, date, descr, section = None, para = None, offset_months = None, offset_weeks = None, offset_days = None):
 
         self.date = date
         self.description = descr
+        self.section = section
+        self.paragraph = para
 
         if offset_months:
             self.date = self.date + relativedelta(months = int(offset_months))
@@ -41,7 +43,11 @@ class meeting_event():
 
         date = datetime.strftime(self.date, '%Y-%m-%d')
 
-        return f'{date} {self.description}'
+        if (self.section and self.paragraph):
+            return f'{date} {self.section} kap. {self.paragraph} {self.description}'
+
+        else:
+            return f'{date} {self.description}'
 
 
     def get_date(self):
@@ -66,6 +72,8 @@ class lbac_meet:
         # 3 kap 1§ stycke 2
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Kallelse till årsmötet och förslag till föredragningslista.',
+                                     section = 3,
+                                     para = 1,
                                      offset_weeks = -4
                                      ))
 
@@ -73,12 +81,16 @@ class lbac_meet:
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Verksamhetsberättelse, årsredovisning/årsbokslut, revisorernas berättelser, '
                                      'verksamhetsplan med budget samt styrelsens förslag och inkomna motioner med styrelsens yttrande.',
+                                     section = 3,
+                                     para = 1,
                                      offset_weeks = -1
                                      ))
 
         # 3 kap 2§ stycke 2
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Förslag från medlem (motion) ska vara styrelsen tillhanda senast tre veckor före årsmötet.',
+                                     section = 3,
+                                     para = 2,
                                      offset_weeks = -3
                                      ))
 
@@ -86,12 +98,16 @@ class lbac_meet:
         # 3 kap 4§ stycke 2 punkt 3
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Sista dag för beviljat medlemskap.',
+                                     section = 3,
+                                     para = 4,
                                      offset_months = -2
                                      ))
 
         # 3 kap 4§ stycke 2 punkt 3
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Sista dag för medlemsavgift.',
+                                     section = 3,
+                                     para = 2,
                                      offset_months = -1
                                      ))
 
@@ -100,6 +116,8 @@ class lbac_meet:
                                      descr = 'Valberedningen ska senast två månader före årsmötet tillfråga dem vilkas mandattid utgår '
                                      'vid mötets slut, om de vill kandidera för nästa mandattid. Därefter ska valberedningen '
                                      'informera medlemmarna om eventuella avsägelser.',
+                                     section = 4,
+                                     para = 2,
                                      offset_months = -2
                                      ))
 
@@ -108,6 +126,8 @@ class lbac_meet:
                                      descr = 'Senast en vecka före årsmötet ska valberedningen meddela röstberättigade medlemmar sitt '
                                      'förslag, samt meddela namnen på de personer som i övrigt har föreslagits inför '
                                      'valberedningen.',
+                                     section = 4,
+                                     para = 2,
                                      offset_weeks = -1
                                      ))
 
@@ -115,6 +135,8 @@ class lbac_meet:
         self.add_event(meeting_event(date = self.get_date(),
                                      descr = 'Föreningens räkenskaper för det senaste verksamhets- och räkenskapsåret ska vara '
                                      'revisorerna tillhanda senast en månad före årsmötet.',
+                                     section = 5,
+                                     para = 1,
                                      offset_months = -1
                                      ))
 
@@ -123,6 +145,8 @@ class lbac_meet:
                                      descr = 'Revisorerna ska granska styrelsens förvaltning och räkenskaper för det senaste '
                                      'verksamhets- och räkenskapsåret samt till styrelsen överlämna revisionsberättelse senast '
                                      '14 dagar före årsmötet.',
+                                     section = 5,
+                                     para = 1,
                                      offset_days = -14
                                      ))
 
